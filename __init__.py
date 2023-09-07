9c106602709e57da083ae2c0480994b47798d2ef
Vladimir Kavalerov <vladimir@kavalerov.net>
2023-09-07 18:04:07 +0100

    Add backup executions

--- __init__.py
+++ __init__.py
@@ -8,25 +8,40 @@ from datetime import datetime, timedelta
 
 load_dotenv()
 
+number_of_tries = 15
+sleep_time = 0.5
+
+
+def execute_until_successful(fn):
+    for _ in range(number_of_tries):
+        try:
+            return fn()
+        except Exception:
+            print(_)
+            time.sleep(sleep_time)
+
+
 t = int(os.environ.get("TIME"))
 
 driver = webdriver.Safari()
 driver.get("https://members.wework.com")
-time.sleep(t + 1)
-
-driver.find_element(By.ID, "1-email").send_keys(os.environ.get("EMAIL"))
-driver.find_element(By.ID, "1-password").send_keys(os.environ.get("PASSWORD"))
-driver.find_element(By.ID, "1-submit").click()
+execute_until_successful(
+    lambda: (
+        driver.find_element(By.ID, "1-email").send_keys(os.environ.get("EMAIL")),
+        driver.find_element(By.ID, "1-password").send_keys(os.environ.get("PASSWORD")),
+        driver.find_element(By.ID, "1-submit").click(),
+    )
+)
 time.sleep(2)
 
 driver.get("https://members.wework.com/desks")
-time.sleep(t)
+time.sleep(6)
 
 driver.find_element(
     By.XPATH,
     "//*[@id='root']/div/div/div/div[3]/div[2]/div/div[1]/div/div[1]/div/div/div",
 ).click()
-time.sleep(0.5)
+time.sleep(2)
 
 today_date = datetime.strptime(
     driver.find_element(
@@ -39,28 +54,41 @@ today_date = datetime.strptime(
 )
 
 for i in range(today_date.day, 31):
-    today_date = today_date + timedelta(days=1)
+    today_date: datetime = today_date + timedelta(days=1)
     print(today_date.strftime("%a %b %d %Y"))
-    time.sleep(3)
-    driver.find_element(
-        By.XPATH, f"//*[@id='{os.environ.get('OFFICE_ID')}']/section/button"
-    ).click()
+    execute_until_successful(
+        lambda: driver.find_element(
+            By.XPATH, f"//*[@id='{os.environ.get('OFFICE_ID')}']/section/button"
+        ).click()
+    )
     time.sleep(2)
-    driver.find_element(By.XPATH, "/html/body/div[2]/div/div[2]/span[3]/button").click()
+    execute_until_successful(
+        lambda: driver.find_element(
+            By.XPATH, "/html/body/div[2]/div/div[2]/span[3]/button"
+        ).click()
+    )
     time.sleep(2)
     driver.get("https://members.wework.com/desks")
-    time.sleep(t)
+
+    time.sleep(6)
+    execute_until_successful(
+        lambda:
     driver.find_element(
-        By.XPATH,
-        "//*[@id='root']/div/div/div/div[3]/div[2]/div/div[1]/div/div[1]/div/div/div",
-    ).click()
-    time.sleep(0.5)
-    driver.find_element(
-        By.XPATH,
-        "//*[@id='root']/div/div/div/div[3]/div[2]/div/div[1]/div/div[1]/div/div/div/div[1]/div/div/div/div/div[2]/div/div[3]",
-    ).find_element(
-        By.CSS_SELECTOR, f"div[aria-label='{today_date.strftime('%a %b %d %Y')}']"
-    ).click()
+            By.XPATH,
+            "//*[@id='root']/div/div/div/div[3]/div[2]/div/div[1]/div/div[1]/div/div/div",
+        ).click()
+    )
+    
+    execute_until_successful(
+        lambda: driver.find_element(
+            By.XPATH,
+            "//*[@id='root']/div/div/div/div[3]/div[2]/div/div[1]/div/div[1]/div/div/div/div[1]/div/div/div/div/div[2]/div/div[3]",
+        )
+        .find_element(
+            By.CSS_SELECTOR, f"div[aria-label='{today_date.strftime('%a %b %d %Y')}']"
+        )
+        .click()
+    )
 
 time.sleep(3)
 driver.close()
